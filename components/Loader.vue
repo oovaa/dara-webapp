01219a9dfed03d140e4e0684db7cbea876ccc44b
oovaa <omarvx211@gmail.com>
2025-11-22 18:10:36 +0200

    update loading screen for simplicity at 2025-11-22

--- components/Loader.vue
+++ components/Loader.vue
@@ -9,8 +9,8 @@
                         <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
                     </svg>
                     <div>
-                        <p class="text-blue-800 font-medium">Backend is warming up...</p>
-                        <p class="text-blue-600 text-sm mt-1">This may take a few more seconds as our servers are starting up. Thank you for your patience!</p>
+                        <p class="text-blue-800 font-medium">AI is warming up...</p>
+                        <p class="text-blue-600 text-sm mt-1">This may take a few more seconds as we are starting up. Thank you for your patience!</p>
                     </div>
                 </div>
             </div>
